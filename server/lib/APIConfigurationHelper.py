8ebae01b45cf8082cc6d2c45e9e46b94ea0ac3d2
imallery <ibmallery@gmail.com>
2025-09-14 20:40:50 -0400

    made a configuration helper that loads in environment variables from a .env file and ensures that the environment variables required for the server to successfully use the API are present. As well as provides getters for accessing those environment variables

---
+++ server/lib/APIConfigurationHelper.py
@@ -0,0 +1,60 @@
+from dotenv import load_dotenv
+import os
+
+__all__ = ["helper"]
+
+class APIConfigurationHelper:
+    """
+    This class helps the developer load and access the plaid API environment variables. While in development it will load
+    the variables from the .env file, otherwise it will just access them as normal.
+    """
+    required_keys = ["PLAID_CLIENT_ID","PLAID_SECRET", "PLAID_ENV", "PLAID_PRODUCTS", "PLAID_COUNTRY_CODES"]
+
+    def __init__(self):
+        # load from the .env file
+        load_dotenv()
+
+        # note any required keys that are missing
+        missingRequiredKeys = [key for key in APIConfigurationHelper.required_keys if key not in os.environ]
+        if( len(missingRequiredKeys) != 0 ):
+            raise RuntimeError( f"Missing required environment variables {missingRequiredKeys}. If in development please ensure there is a root-level .env file " +
+                               "with the required keys, if deploying, create the environment variables on your server host" )
+        
+        print("Required Environment variables loaded!", end="\n\n")
+
+    def getPlaidClientId():
+        """
+        Get the plaid client ID for this server's connection to the API. Required for 
+        authenticating this server with the PLAID API
+        """
+        return os.environ.get("PLAID_CLIENT_ID")
+
+    def getPlaidSecret():
+        """
+        Get the plaid secret for this server's connection to the API. Required for 
+        authenticating this server with the PLAID API
+        """
+        return os.environ.get("PLAID_SECRET")
+    
+    def getPlaidEnv():
+        """
+        Get the plaid environment that this server is using. Used to tell 
+        the server if you arer going to use actual data or test data.
+        """
+        return os.environ.get("PLAID_ENV")
+    
+    def getPlaidProducts():
+        """
+        Get the plaid products that this server can access. Used to tell the server
+        what API services it wishes to use
+        """
+        return os.environ.get("PLAID_PRODUCTS")
+    
+    def getPlaidCountryCodes():
+        """
+        Get the plaid country codes that this server can use. Used to determine which
+        countries this server may be requesting data for.
+        """
+        return os.environ.get("PLAID_COUNTRY_CODES")
+    
+helper = APIConfigurationHelper()
--- server/plaid_server.py
+++ server/plaid_server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from lib.APIConfigurationHelper import helper
 
 app = Flask(__name__)
 
