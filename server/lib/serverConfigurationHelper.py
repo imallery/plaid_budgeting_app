3a950b59fc4ca8a6f00836285c9f1aed58bd7628
imallery <ibmallery@gmail.com>
2025-11-23 22:10:53 -0500

    Set up a dev container inside of vscode for future development efforts. Changed the name of the APICOnfigurationHelper to better fit what it is doing, also did some small updated to make the few methods that can return comma-delimited strings to return a list instead of a string. Then added the client object intialization to the plaid_server.py

--- server/lib/serverConfigurationHelper.py
+++ server/lib/serverConfigurationHelper.py
@@ -1,9 +1,9 @@
 from dotenv import load_dotenv
 import os
 
-__all__ = ["helper"]
+__all__ = ["serverConfig"]
 
-class APIConfigurationHelper:
+class serverConfigurationHelper:
     """
     This class helps the developer load and access the plaid API environment variables. While in development it will load
     the variables from the .env file, otherwise it will just access them as normal.
@@ -22,39 +22,39 @@ class APIConfigurationHelper:
         
         print("Required Environment variables loaded!", end="\n\n")
 
-    def getPlaidClientId():
+    def getPlaidClientId(self) -> str:
         """
         Get the plaid client ID for this server's connection to the API. Required for 
         authenticating this server with the PLAID API
         """
         return os.environ.get("PLAID_CLIENT_ID")
 
-    def getPlaidSecret():
+    def getPlaidSecret(self) -> str:
         """
         Get the plaid secret for this server's connection to the API. Required for 
         authenticating this server with the PLAID API
         """
         return os.environ.get("PLAID_SECRET")
     
-    def getPlaidEnv():
+    def getPlaidEnv(self) -> str:
         """
         Get the plaid environment that this server is using. Used to tell 
         the server if you arer going to use actual data or test data.
         """
         return os.environ.get("PLAID_ENV")
     
-    def getPlaidProducts():
+    def getPlaidProducts(self) -> list[str]:
         """
         Get the plaid products that this server can access. Used to tell the server
         what API services it wishes to use
         """
-        return os.environ.get("PLAID_PRODUCTS")
+        return os.environ.get("PLAID_PRODUCTS").split(",")
     
-    def getPlaidCountryCodes():
+    def getPlaidCountryCodes(self) -> list[str]:
         """
         Get the plaid country codes that this server can use. Used to determine which
         countries this server may be requesting data for.
         """
-        return os.environ.get("PLAID_COUNTRY_CODES")
+        return os.environ.get("PLAID_COUNTRY_CODES").split(",")
     
-helper = APIConfigurationHelper()
+serverConfig = serverConfigurationHelper()
--- server/plaid_server.py
+++ server/plaid_server.py
@@ -1,5 +1,28 @@
 from flask import Flask
-from lib.APIConfigurationHelper import helper
+from lib.serverConfigurationHelper import serverConfig
+import plaid
+from plaid.api import plaid_api
+
+# create the configuration object for our plaid api connection
+configuration = plaid.Configuration(
+    host= serverConfig.getPlaidEnv(),
+    api_key= {
+        "client_id": serverConfig.getPlaidClientId(),
+        "secret": serverConfig.getPlaidSecret()
+    }
+)
+
+# use the client object to send future requests to the API, it will automatically
+# include all of our configuration info
+api_client = plaid.ApiClient(configuration)
+client = plaid_api.PlaidApi(api_client)
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def baseRequest():
+    return "This endpoint is not configured for use, please use an intended API route", 400
+
+@app.route("/request-link", methods=["GET"])
+def requestLink():
+    pass
